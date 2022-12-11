5c62581591312d0435333e47c09e5d379c30a0a6
katie <katiepb525@gmail.com>
2022-12-11 16:47:57 -0600

    cleanup

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
+require 'pry-byebug'
 # binary tree node
 class Node
   include Comparable
   attr_accessor :left, :right, :value
-  def initialize(left=nil, right=nil, value)
+
+  def initialize(value, left = nil, right = nil)
+    @value = value
     @left = left
     @right = right
-    @value = value
   end
 end
 
@@ -21,16 +23,15 @@ class Tree
   # input: sorted array
   # output: root node of balanced BST
   def build_tree(arr, t_start, t_end)
-
     return if arr.class != Array
 
-    return if (t_start > t_end)
+    return if t_start > t_end
 
     # find middle index of arr
     mid = (t_start + t_end) / 2
 
     # make middle element root
-    root = Node.new(nil, nil, arr[mid])
+    root = Node.new(arr[mid])
 
     # assign all values < mid to left of subtree
     root.left = build_tree(arr, t_start, mid - 1)
@@ -42,15 +43,15 @@ class Tree
     root
   end
 
+  # print out search tree
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
     pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
-
 end
 
-arr = [1,2,3,4,5,6,7]
+arr = [1, 2, 3, 4, 5, 6, 7]
 
 tree = Tree.new(arr)
 tree.pretty_print
