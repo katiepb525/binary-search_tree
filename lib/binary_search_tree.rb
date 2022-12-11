d82efa853f88cfda6416407e074d2e1df0a8ea38
katie <katiepb525@gmail.com>
2022-12-11 16:40:54 -0600

    add pretty print method from odin

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -36,6 +36,12 @@ class Tree
     root.right = build_tree(arr[mid+1])
 
     # return root element
-    return root
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.value}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
